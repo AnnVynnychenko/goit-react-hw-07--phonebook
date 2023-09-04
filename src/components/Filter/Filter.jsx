be602185f7faf40b711b442ebd59f53de69d26b0
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-09-04 13:19:07 +0300

    hw-07

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 import './Filter.css';
 // import { contactsFiltered, getFilterValue } from 'redux/contactSlice';
 
